e35cb2fb54208418d00de3105f2b4429ed480454
MohamedKassem95 <80948494+MohamedEmadKassem@users.noreply.github.com>
2021-05-10 23:19:31 +0300

    CS439ForumAndTestSuites

---
+++ Test Suites/Interaction.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Interaction</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ff368f99-0475-4488-9c62-1c8698386c41</testSuiteGuid>
+</TestSuiteEntity>
