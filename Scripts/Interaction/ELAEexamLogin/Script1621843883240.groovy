1369a62221a8e5450897ad68f3c2f93674ffca45
MohamedKassem95 <80948494+MohamedEmadKassem@users.noreply.github.com>
2021-05-24 11:21:16 +0300

    Interaction

---
+++ Scripts/Interaction/ELAEexamLogin/Script1621843883240.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://sucourse.sabanciuniv.edu/plus/')
+
+WebUI.click(findTestObject('ELAEexamLogin/Page_SUCourse/a_Login'))
+
+WebUI.click(findTestObject('ELAEexamLogin/Page_SUCourse Authentication choice/a_ELAE exam login'))
+
+WebUI.setText(findTestObject('ELAEexamLogin/Page_SUCourse Log in to the site/input_Username_username'), 'mohamedkassem')
+
+WebUI.setEncryptedText(findTestObject('ELAEexamLogin/Page_SUCourse Log in to the site/input_Password_password'), 'lsh4UT/KYs157Zjg3xgr3w==')
+
+WebUI.click(findTestObject('ELAEexamLogin/Page_SUCourse Log in to the site/input_Password_loginbtn'))
+
+WebUI.verifyTextPresent('Invalid login, please try again', false)
+
